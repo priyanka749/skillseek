48c762482e4d92c1e665d5ef2519f8c2da042e9f
priyanka749 <priyankabhandari749@gmail.com>
2025-01-16 22:38:23 +0545

    added auth code

--- lib/features/auth/domain/entity/auth_entity.dart
+++ lib/features/auth/domain/entity/auth_entity.dart
@@ -2,22 +2,31 @@ import 'package:equatable/equatable.dart';
 
 class AuthEntity extends Equatable {
   final String? userId;
-  final String fullName;
+  final String username;
   final String email;
   final String phoneNumber;
   final String? address;
   final String password;
+  final String confirmPassword;
 
   const AuthEntity({
     this.userId,
-    required this.fullName,
+    required this.username,
     required this.email,
     required this.phoneNumber,
-    this.address,
+    required this.address,
     required this.password,
+    required this.confirmPassword,
   });
 
   @override
-  List<Object?> get props =>
-      [userId, fullName, email, phoneNumber, address, password];
+  List<Object?> get props => [
+        userId,
+        username,
+        email,
+        phoneNumber,
+        address,
+        password,
+        confirmPassword
+      ];
 }
--- lib/features/auth/domain/repository/auth_entity.dart
+++ lib/features/auth/domain/repository/auth_entity.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:dartz/dartz.dart';
 import 'package:skillseek/core/error/failure.dart';
 import 'package:skillseek/features/auth/domain/entity/auth_entity.dart';
@@ -8,8 +6,6 @@ abstract interface class IAuthRepository {
   Future<Either<Failure, void>> register(AuthEntity user);
 
   Future<Either<Failure, String>> userlogin(String username, String password);
-
-  Future<Either<Failure, String>> uploadProfilePicture(File file);
 
   Future<Either<Failure, AuthEntity>> getCurrentUser();
 }
